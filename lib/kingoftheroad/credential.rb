4d6122cbecd42bf8bca6a6199ca307ff30f4a6e2
Jeff Juszczak <jeff@moottoast.com>
2017-05-05 19:59:09 -0400

    WIP - credential.rb

---
+++ lib/kingoftheroad/credential.rb
@@ -0,0 +1,20 @@
+module Kingoftheroad
+  class Credential
+    include Methadone::CLILogging
+    include Methadone::SH
+    include Methadone::Main
+    
+    attr_reader :access_key_id, :secret_access_key
+
+    def initialize(credential_file)
+      @credential_file = credential_file
+
+      credential_array = CSV.read(@credential_file)
+
+      @access_key_id = credential_array[1][0]
+      @secret_access_key = credential_array[1][1]
+    end
+
+    
+  end
+end
